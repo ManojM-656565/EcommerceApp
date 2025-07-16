db4f150623f8f8cd2052d68d83afc9a40475d4bc
ManojM-656565 <137311406+ManojM-656565@users.noreply.github.com>
2025-07-16 13:33:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    // <div>
+      <div>
+        <h1 className="text-3xl font-bold underline bg-color-primaryblue">
+    Hello world!
+         </h1>
+
+      </div>
+    
+  )
+}
+
+export default App
