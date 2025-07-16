74121c9426fd1714f3e5383d78fca8bd8cbf75a7
ManojM-656565 <137311406+ManojM-656565@users.noreply.github.com>
2025-07-16 13:46:28 +0530

    graph ql setup commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
